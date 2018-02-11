49f26fad0b43803039957c0947d77afa901cc107
Brad <Brad@bradboughn.com>
2018-02-11 17:07:13 -0500

    More Renderer optimization. Added Clipping/Straight up don't render conditions

--- src/com/bradboughn/lightnetengine/Renderer.java
+++ src/com/bradboughn/lightnetengine/Renderer.java
@@ -5,6 +5,7 @@ import com.bradboughn.lightnetengine.gfx.GameImage;
 import java.awt.image.DataBufferInt;
 
 public class Renderer {
+        private static int counter; //TEST var
         private int pixelW, pixelH;
         private int[] pixels;
         
@@ -32,15 +33,35 @@ public class Renderer {
             pixels[x + (y*pixelW)] = value;
         }
         
+        /*  
+        *   Regarding the Clipping code below:
+        *   Stops from trying to set pixel data for images, or parts of images, which would be past our canvas, thus not actually drawn anyway
+        *   It does this by using these conditions' new values and applying them to our loop below which uses 
+        *   these new values to say: "this newWidth/newHeight" are the amount of pixels we need to actually draw" or for newX/newY : "These values are where
+        *   we need to start from, in our particular image array"
+        */
+        
         public void drawImage(GameImage image, int offsetX, int offsetY) {
+            if (counter>180) counter = 0;
+            counter++;
             int newX = 0;
             int newY = 0;
             int newWidth = image.getWidth();
             int newHeight = image.getHeight();
             
-            for (int y = 0; y < image.getHeight(); y++) {
+            //Don't Render code
+            if (offsetX < -newWidth) return;
+            if (offsetY < -newHeight)return;
+            
+            //Clipping code
+            if (offsetX < 0) newX -= offsetX;       
+            if (offsetY < 0) newY -= offsetY;       
+            if (newWidth + offsetX  > pixelW) newWidth -= newWidth + offsetX - pixelW;
+            if (newHeight + offsetY > pixelH) newHeight -= newHeight + offsetY - pixelH;
+            
+            for (int y = newY; y < newHeight; y++) {
              
-                for (int x = 0; x < image.getWidth(); x++) {
+                for (int x = newX; x < newWidth; x++) {
                     setPixel(x + offsetX, y + offsetY, image.getPixels()[x + (y*image.getWidth())]);
                 }
             }
